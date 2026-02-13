667e5404ef11dabcd9a34f0cbc5e6de02421eb7c
Jared Peña Ochoa <jared@jrdx0.dev>
2026-02-13 12:46:09 -0600

    section 05 done

---
+++ 04-blog/src/config/site-config.ts
@@ -0,0 +1,4 @@
+export const SITE_CONFIG = {
+    title: "Jared's blog",
+    description: "Blog oficial de Jared Peña - Full Stack Developer"
+}
---
+++ 04-blog/src/utils/formatter.ts
@@ -0,0 +1,11 @@
+export class Formatter {
+    static date(value: Date): string {
+        const date = new Date(value);
+
+        return Intl.DateTimeFormat('es-MX', {
+            year: "numeric",
+            month: "long",
+            day: "2-digit"
+        }).format(date)
+    }
+}
