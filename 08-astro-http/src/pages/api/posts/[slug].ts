0dc202caa8c0edd6c9c6f69528a17838f31430e1
Jared Peña Ochoa <jared@jrdx0.dev>
2026-02-26 22:01:39 -0600

    section 09 done

---
+++ 08-astro-http/src/pages/api/posts/[slug].ts
@@ -0,0 +1,76 @@
+import type { APIRoute, GetStaticPaths } from "astro";
+import { getEntry } from "astro:content";
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const { slug } = params;
+
+    const post = await getEntry('blog', slug as any);
+
+    if (!post) {
+        return new Response(JSON.stringify({ error: `Post ${slug} not found` }), {
+            status: 404,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    return new Response(JSON.stringify(post), {
+        status: 200, 
+        headers: {
+            'Content-Type': 'application/json'
+        } 
+    });
+}
+
+export const POST: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    
+    return new Response(JSON.stringify({ method: 'POST', ...body }), {
+        status: 200, 
+        headers: {
+            'Content-Type': 'application/json'
+        } 
+    });
+}
+
+export const PUT: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    
+    return new Response(JSON.stringify({ method: 'PUT', ...body }), {
+        status: 200, 
+        headers: {
+            'Content-Type': 'application/json'
+        } 
+    });
+}
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    
+    return new Response(JSON.stringify({ method: 'PATCH', ...body }), {
+        status: 200, 
+        headers: {
+            'Content-Type': 'application/json'
+        } 
+    });
+}
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+    const body = await request.json();
+    
+    return new Response(JSON.stringify({ method: 'DELETE', ...body }), {
+        status: 200, 
+        headers: {
+            'Content-Type': 'application/json'
+        } 
+    });
+}
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     return [
+//         {
+//             params: { slug: 'first-post' }
+//         }
+//     ]
+// }
